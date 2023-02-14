d0757beba41442690313ff6c33ca4ffe0306a26c
aeolus-1 <121691@student.hillbrook.qld.edu.au>
2023-02-14 11:06:08 +1000

    w

--- sound.js
+++ sound.js
@@ -18,14 +18,10 @@ class SoundController {
                 "shoot2",
                 "shoot3",
             ]*/
-<<<<<<< HEAD
-        }[soundName]||["yya"]
-=======
             jump: ["jump.wav"],
             gun: ["laserShoot.wav", "laserShoot (1).wav", "laserShoot (2).wav", "laserShoot (3).wav", "laserShoot (4).wav", "laserShoot (5).wav", "laserShoot (6).wav"],
             explosion: ["explosion.wav"]
         }[soundName]
->>>>>>> e0e0bfcbcecc058c5953d727fa8796841afe784d
         var link = link[randInt(0,link.length-1)],
             audio = new Audio("sfx/"+link);
         audio.play();    
