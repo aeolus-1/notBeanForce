8d5ec91d1859417e7e51200e6591e223c7be6340
Aiden Venter <aiden.venter@gmail.com>
2023-02-14 10:58:52 +1000

    sound thing

--- sound.js
+++ sound.js
@@ -6,6 +6,9 @@ class SoundController {
                 "shoot2",
                 "shoot3",
             ]*/
+            jump: ["jump.wav"],
+            gun: ["laserShoot.wav"],
+            explosion: ["explosion.wav"]
         }[soundName]
         var link = link[randInt(0,link.length-1)],
             audio = new Audio("sfx"+link);
